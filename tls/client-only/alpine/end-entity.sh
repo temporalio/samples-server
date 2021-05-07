fb0bd6806b43926c88a738dee59f867c99cf0e9f
Lanie Hei <lanie.b.hei@gmail.com>
2021-05-07 11:57:20 -0500

    Add client-only alpine linux and mac scripts to generate certificates (#10)
    * Add client-only alpine linux and mac scripts
    
    * Fix comments
    
    * Remove uppercase letters from DNS configuration
    
    * Add docker image reference to README
    
    * Remove unecessary note
    
    * Add production precaution

---
+++ tls/client-only/alpine/end-entity.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Enter a unique name for the prefix of the .pem and .key files":
+read UNIQUE_NAME
+echo "Generating private key and CSR"
+
+# Grep other conf file to grab company name.
+DNS=$(grep DNS.1 ca.conf)
+
+# Find the company name e.g. from `DNS.1=client.root.<company name>.<rand values>` 
+IFS="." read -a strarr <<< "${DNS}"
+CN="${strarr[3]}"
+
+# Return a random stream of data, fold makes a new line every 4 characters, head will take the first line. 
+RANDLETTER=$(cat /dev/urandom | busybox tr -dc 'a-z0-9' | busybox fold -w 4 | busybox head -n 1)
+
+# Enter this as the DNS. 
+DNS_END_ENTITY="client.endentity.${CN}.${RANDLETTER}"
+
+GENERATED_END_ENTITY_CONF_FILE="${UNIQUE_NAME}.conf"
+cat << EOF > ${GENERATED_END_ENTITY_CONF_FILE}
+[req]
+default_bits = 4096
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+prompt = no
+
+[req_ext]
+subjectAltName = @alt_names
+
+[dn]
+O = $CN
+
+[alt_names]
+DNS.1 = $DNS_END_ENTITY
+EOF
+
+# Generate client's private key and certificate signing request (CSR)
+openssl req -newkey rsa:4096 -nodes -keyout "${UNIQUE_NAME}.key" -out "${UNIQUE_NAME}-req.pem" -config "${UNIQUE_NAME}.conf"
+
+echo "Signing certificate"
+
+# Use CA's private key to sign client's CSR and get back the signed certificate
+openssl x509 -days 365 -req -in "${UNIQUE_NAME}-req.pem" -CA "ca.pem" -CAkey "ca.key" -CAcreateserial -out "${UNIQUE_NAME}.pem" -extfile "${UNIQUE_NAME}.conf"
+
+# Delete the certificate signing request after the certificate has been signed. 
+rm "${UNIQUE_NAME}-req.pem"
+
+echo "Printing signed end-entity certificate"
+openssl x509 -in "${UNIQUE_NAME}.pem" -noout -text
+
+echo "---> Keep these files secure, and use ${UNIQUE_NAME}.pem and ${UNIQUE_NAME}.key in the SDK"
---
+++ tls/client-only/mac/ca.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "Creating the CSR (certificate signing request)"
+
+## This will also be used as part of the DNS for the CA and end-entity in addition to the organization setting.
+echo "What is your company name?:"
+read COMPANY_NAME
+
+# Lower case company name for DNS.
+CN=$(echo $COMPANY_NAME | awk '{print tolower($0)}')
+
+## Return a random stream of data, fold makes a new line every 4 characters, head will take the first line. 
+RANDLETTER=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-z0-9' | fold -w 4 | head -n 1)
+DNS_ROOT="client.root.${CN}.${RANDLETTER}"
+
+touch ca.conf
+chmod 0755 ca.conf 
+
+GENERATED_ROOT_CONF_FILE="ca.conf"
+cat << EOF > ${GENERATED_ROOT_CONF_FILE}
+[req]
+default_bits = 4096
+default_md = sha256
+req_extensions = req_ext
+x509_extensions = v3_ca
+distinguished_name = dn
+prompt = no
+
+[v3_ca]
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid:always,issuer
+basicConstraints = critical,CA:TRUE,pathlen:0
+keyUsage = critical,digitalSignature,cRLSign,keyCertSign
+
+[req_ext]
+subjectAltName = @alt_names
+
+[dn]
+O = $COMPANY_NAME
+
+[alt_names]
+DNS.1 = $DNS_ROOT
+
+EOF
+
+openssl genrsa -out ca.key 4096
+chmod 0755 ca.key 
+
+openssl req -new -x509 -days 365 -key ca.key -out ca.pem -config ca.conf
+chmod 0755 ca.pem 
+
+echo "Printing root CA certificate"
+openssl x509 -in ca.pem -noout -text
+
+echo "---> Keep ca.key and ca.pem secure, but available for generating end entities in the future"
