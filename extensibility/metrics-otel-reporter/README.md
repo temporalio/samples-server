b259c2a22a82831c8e4e6df928d2e9ebd103f79c
Mikhail Gryzykhin <mikhail@temporal.io>
2022-02-18 11:04:42 -0800

    Add sample for overriding otel reporter

---
+++ extensibility/metrics-otel-reporter/README.md
@@ -0,0 +1,15 @@
+### Metrics reporter
+
+This sample shows how to inject a custom reporter that
+controls metrics reporting.  
+
+### Steps to run this sample
+1. Start up the dependencies by running the `make start-dependencies` command from within the main Temporal repository
+as described in the 
+[contribution guide](https://github.com/temporalio/temporal/blob/master/CONTRIBUTING.md#runing-server-locally).
+
+2. Create the database schema by running `make install-schema`.
+
+3. Start Temporal by running `go run metrics-reporter/server/main.go`.
+
+4. Metrics will be output to console.
---
+++ extensibility/metrics-otel-reporter/server/main.go
@@ -0,0 +1,63 @@
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package main
+
+import (
+	"context"
+
+	"go.temporal.io/server/common/log"
+	"go.temporal.io/server/common/metrics"
+	"go.temporal.io/server/temporal"
+
+	metrics_reporter "github.com/temporalio/service-samples/metrics-reporter"
+)
+
+func main() {
+	ctx := context.Background()
+	logger := log.NewCLILogger()
+	mustProvider, err := metrics_reporter.NewOpentelemeteryReporter(logger)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	reporter, err := metrics.NewOpentelemeteryReporter(logger, &metrics.ClientConfig{}, mustProvider)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	s := temporal.NewServer(
+		temporal.ForServices(temporal.Services),
+		temporal.WithConfigLoader("./metrics-reporter/config", "development", ""),
+		temporal.InterruptOn(temporal.InterruptCh()),
+		temporal.WithCustomMetricsReporter(reporter),
+	)
+
+	mustProvider.GetMeterMust().NewInt64Counter("test").Add(ctx, 11)
+
+	err = s.Start()
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+}
