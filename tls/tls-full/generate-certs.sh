cda270976368be248d6b370cadba26a182648965
Ryan Goulding <goulding@layerzerolabs.org>
2022-07-11 12:10:50 -0700

    Fix a small typo
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

--- tls/tls-full/generate-certs.sh
+++ tls/tls-full/generate-certs.sh
@@ -1,5 +1,5 @@
 # This scripts generates test keys and certificates for the sample.
-# In a production environment such artifacts should be genrated
+# In a production environment such artifacts should be generated
 # by a proper certificate authority and handled in a secure manner.
 
 CERTS_DIR=./certs
